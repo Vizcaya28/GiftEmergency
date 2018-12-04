8306f39de4c0028e47914a5349ca9ab9665e0afd
Jesús Vizcaya <40042628+Vizcaya28@users.noreply.github.com>
2018-12-04 13:24:31 -0600

    Add files via upload

--- Gift_Emergency/Index.php
+++ Gift_Emergency/Index.php
@@ -13,26 +13,18 @@
 <body>
 	<div class="header">
 		<div class="row">
-			<nav class="pushpin-demo-nav col s12 red darken-4">
-				<a href="#" class="brand-logo " style="font-family: 'Pacifico';">Gift Emergency</a>
-				<a href="#" class="brand-logo center"><img src="css/img/icon-logo-medium.png"></a>
+			<nav class="col s12 transparent">
+				<a href="#" class="brand-logo " style="font-family: 'Pacifico'; color: #673ab7;">Gift Emergency</a>
+				<a href="#" class="brand-logo center"><img src="css/img/icon-logo-medium.png" width="64px"></a>
 				<ul class="right hide-on-med-and-down">
-				   	<li><a href="register.php" class="btn red darken-4 z-depth-1" >Registro</a></li>
-		        	<li><a href="#"class="btn red darken-4 waves-effect waves-yellow z-depth-2 ">Iniciar Sesión</a></li>
+				   	<li><a href="register.php" class="btn deep-purple z-depth-1 white-text" >Registro</a></li>
+		        	<li><a href="#"class="btn deep-purple z-depth-2 ">Iniciar Sesión</a></li>
 		        </ul>
 			</nav>
 		</div>
-		<div class="row">
-			<div class="col s12">
-			    <div class="card z-depth-2">
-				    <div class="card-image">
-			    	    <img  src="css/img/wall.jpg" height="750px;" width="80%">
-			        	<span class="card-title"><p  class="flow-text" style="color: #B71C1C;">El regalo ideal, para la personal ideal!...</p></span>
-			        </div>
-			    </div>	
-			</div>
-		</div>
-	<footer class="page-footer red darken-4">
+		
+		
+	<footer class="page-footer deep-purple">
           <div class="container">
             <div class="row">
               <div class="col l6 s12 ">
