9ef50a26c92c25ab210fbbe6cd5eec3bf9be6735
Jesús Vizcaya <40042628+Vizcaya28@users.noreply.github.com>
2018-12-03 21:40:33 -0600

    Add files via upload

---
+++ Gift_Emergency/register.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gift Emergency</title>
+	<!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!-- Import Google Fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+	<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        
+</head>
+<body>
+	<div class="header">
+		<div class="row">
+			<nav class="pushpin-demo-nav col s12 red darken-4">
+				<a href="#" class="brand-logo " style="font-family: 'Pacifico';">Gift Emergency</a>
+				<a href="#" class="brand-logo center"><img src="css/img/icon-logo-medium.png"></a>
+				<ul class="right hide-on-med-and-down">
+				   	<li><a href="#" class="btn red darken-4 z-depth-1" >Registro</a></li>
+		        	<li><a href="#"class="btn red darken-4 waves-effect waves-yellow z-depth-2 ">Iniciar Sesión</a></li>
+		        </ul>
+			</nav>
+		</div>
+		<div class="row">
+			<div class="col s12">
+			    <div class="card z-depth-2">
+				    <div class="card-image">
+			    	    <img  src="css/img/wall.jpg" height="750px;" width="80%">
+			        	<span class="card-title"><p  class="flow-text" style="color: #B71C1C;">El regalo ideal, para la personal ideal!...</p></span>
+			        </div>
+			    </div>	
+			</div>
+		</div>
+	<footer class="page-footer red darken-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12 ">
+                <h5 class="white-text">Gift Emergency</h5>
+                <p class="">Proyecto Final. Desarrollo Web</p>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            	<p>© All Rights Reserved by Equipo 2</p>
+            </div>
+          </div>
+    </footer>
+	</div>
+	<!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>  
+</body>
+</html>
